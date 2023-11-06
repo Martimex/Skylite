b8999e612bacddf4434f33ebf31d15791b03d72c
Martimex <kamilmarciniak533@wp.pl>
2023-11-06 12:12:58 +0100

    Update for an OfferShowcase components
    The OfferShowcase component received bunch of scroll animations that
    make the section feel more alive and interactive. The necessary setup
    is almost done, with an exception of content boxes, which has to be
    properly distributed in terms of their content and appear animation.

--- src/components/OfferShowcase.vue
+++ src/components/OfferShowcase.vue
@@ -1,9 +1,175 @@
 <script setup lang="ts">
 
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/src/ScrollTrigger';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    /* 1 */
+
+    gsap.to(`.offer-item[data-no="1"] .offer-content`, {
+        scrollTrigger: {
+            trigger: `.offer-item[data-no="1"]`,
+            endTrigger: `.offer-item[data-no="1"]`,
+            start: '10% 80%',
+            end: '40% 40%',
+            /* markers: true, */
+            scrub: true,
+            toggleActions: 'play pause reverse pause',
+        },
+        backgroundImage: 'linear-gradient(135deg, #222, #111)',
+    });
+
+    gsap.to(`.offer-item[data-no="1"] .offer-dash`, {
+        scrollTrigger: {
+            trigger: `.offer-item[data-no="1"]`,
+            endTrigger: `.offer-item[data-no="1"]`,
+            start: '0% 30%',
+            end: '35% 10%',
+            /* markers: true, */
+            scrub: true,
+            toggleActions: 'play pause reverse pause',
+        },
+        height: '100%',
+    });
+
+
+    /* 2 */
+
+    gsap.to(`.offer-item[data-no="2"] .offer-content`, {
+        scrollTrigger: {
+            trigger: `.offer-item[data-no="2"]`,
+            endTrigger: `.offer-item[data-no="2"]`,
+            start: '10% 80%',
+            end: '40% 40%',
+            /* markers: true, */
+            scrub: true,
+            toggleActions: 'play pause reverse pause',
+        },
+        /* background:'#222', */
+        backgroundImage: 'linear-gradient(135deg, #222, #111)',
+        /* width: '80vw', */
+        /* height: '100vh', */
+        /* borderRadius: '2rem', */
+    });
+
+    gsap.to(`.offer-item[data-no="2"] .offer-dash`, {
+        scrollTrigger: {
+            trigger: `.offer-item[data-no="2"]`,
+            endTrigger: `.offer-item[data-no="2"]`,
+            start: '0% 30%',
+            end: '35% 10%',
+            /* markers: true, */
+            scrub: true,
+            toggleActions: 'play pause reverse pause',
+        },
+        height: '100%',
+    });
+
+
+    /* 3 */
+
+    gsap.to(`.offer-item[data-no="3"] .offer-content`, {
+        scrollTrigger: {
+            trigger: `.offer-item[data-no="3"]`,
+            endTrigger: `.offer-item[data-no="3"]`,
+            start: '10% 80%',
+            end: '40% 40%',
+            /* markers: true, */
+            scrub: true,
+            toggleActions: 'play pause reverse pause',
+        },
+        backgroundImage: 'linear-gradient(135deg, #222, #111)',
+    });
+
+    gsap.to(`.offer-item[data-no="3"] .offer-dash`, {
+        scrollTrigger: {
+            trigger: `.offer-item[data-no="3"]`,
+            endTrigger: `.offer-item[data-no="3"]`,
+            start: '0% 30%',
+            end: '35% 10%',
+            /* markers: true, */
+            scrub: true,
+            toggleActions: 'play pause reverse pause',
+        },
+        height: '100%',
+    });
+
+
+    /* 4 */
+
+    gsap.to(`.offer-item[data-no="4"] .offer-content`, {
+        scrollTrigger: {
+            trigger: `.offer-item[data-no="4"]`,
+            endTrigger: `.offer-item[data-no="4"]`,
+            start: '10% 80%',
+            end: '40% 40%',
+            /* markers: true, */
+            scrub: true,
+            toggleActions: 'play pause reverse pause',
+        },
+        backgroundImage: 'linear-gradient(135deg, #222, #111)',
+    });
+
+    gsap.to(`.offer-item[data-no="4"] .offer-dash`, {
+        scrollTrigger: {
+            trigger: `.offer-item[data-no="4"]`,
+            endTrigger: `.offer-item[data-no="4"]`,
+            start: '0% 30%',
+            end: '35% 10%',
+            /* markers: true, */
+            scrub: true,
+            toggleActions: 'play pause reverse pause',
+        },
+        height: '100%',
+    });
+    
+})
+
 </script>
 
 <template>
     <p class="section-title text-center text-blue-400 font-bold text-5xl tracking-wide my-16"> How can we help you </p>
+
+    <div class="offer-all">
+        <div class="offer-item" data-no="1">
+            <div class="offer-circle"></div>
+            <div class="offer-dash"></div>
+            <p class="offer-semititle"> ONLINE ADVERTISING </p>
+            <div class="offer-content">
+
+            </div>
+        </div>
+        <div class="offer-item" data-no="2">
+            <div class="offer-circle"></div>
+            <div class="offer-dash"></div>
+            <p class="offer-semititle"> SEO PRINCIPLES </p>
+            <div class="offer-content">
+                
+            </div>
+        </div>
+        <div class="offer-item" data-no="3">
+            <div class="offer-circle"></div>
+            <div class="offer-dash"></div>
+            <p class="offer-semititle"> BRAND AWARENESS </p>
+            <div class="offer-content">
+                
+            </div>
+        </div>
+        <div class="offer-item" data-no="4">
+            <div class="offer-circle"></div>
+            <div class="offer-dash"></div>
+            <p class="offer-semititle"> MARKETING EVENTS </p>
+            <div class="offer-content">
+                
+            </div>
+        </div>
+
+    </div>
+
 
     <div class="flex items-center justify-evenly h-screen">
 
@@ -43,6 +209,132 @@
 </template>
 
 <style scoped>
+
+    /* NEW STYLES */
+
+    .offer-all {
+        /* background: #111; */
+        margin-inline: 10vw;
+        min-height: 50vh;
+    }
+
+    .offer-item {
+        --green: hsl(102, 80%, 78%);
+        --green-lite: hsla(102, 100%, 68%, .5);
+        --yellow: hsl(33, 80%, 78%);
+        --yellow-lite: hsla(33, 100%, 68%, .5);
+        --blue: hsl(177, 80%, 78%);
+        --blue-lite: hsla(177, 100%, 68%, .5);
+        --red: hsl(3, 80%, 78%);
+        --red-lite: hsla(3, 100%, 68%, .5);
+
+        font-size: 1rem;
+        padding-bottom: 15vh;
+        /* border-bottom: 2.5em solid pink; */
+        position: relative;
+       /*  margin-bottom: 50vh;  EITHER 50vh or 100vh */
+    }
+
+    .offer-circle {
+        --padding-value: 3em;
+        font-size: 1rem;
+        position: absolute;
+        /* top: calc(50% - var(--padding-value)); */
+        top: 0;
+        padding: var(--padding-value);
+        border-radius: 4rem;
+        background: #222;
+    }
+
+    .offer-dash {
+        font-size: 1rem;
+        position: absolute;
+        top: 6em;
+        padding: .12em;
+        height: 0%; /* GSAP will animate this back to 100% */
+        margin-left: calc(3em - .12em);
+        border-radius: 9999px;
+    }
+
+    .offer-semititle {
+        font-size: 4rem;
+        margin-left: 2.5em;
+        font-weight: 800;
+        color: #000;
+        display: inline-block;
+        /* padding-block: 20vh; */
+    }
+
+    .offer-content {
+        font-size: 1rem;
+        margin-top: 5vh;
+        margin-left: 6em;
+        min-height: 75vh;
+        /* background: #2220; */
+        background-image: linear-gradient(45deg, #2221, #1111);
+        border-radius: 1em;
+    }
+
+    .offer-icon {
+        font-size: 2rem;
+    }
+
+
+    .offer-item[data-no="1"] .offer-circle {
+        background-image: radial-gradient(#000 55%, var(--green));
+        box-shadow: 0 0 1.2em .3em var(--green);
+    }
+
+    .offer-item[data-no="1"] .offer-dash {
+        background: linear-gradient(135deg, var(--green-lite), var(--yellow-lite));
+    }
+
+    .offer-item[data-no="1"] .offer-semititle {
+        -webkit-text-stroke: .05rem var(--green);
+    }
+
+    .offer-item[data-no="2"] .offer-circle {
+        background-image: radial-gradient(#000 55%, var(--yellow));
+        box-shadow: 0 0 1.2em .3em var(--yellow);
+    }
+
+    .offer-item[data-no="2"] .offer-dash {
+        background: linear-gradient(135deg, var(--yellow-lite), var(--blue-lite));
+    }
+
+    .offer-item[data-no="2"] .offer-semititle {
+        -webkit-text-stroke: .05rem var(--yellow);
+    }
+
+    .offer-item[data-no="3"] .offer-circle {
+        background-image: radial-gradient(#000 55%, var(--blue));
+        box-shadow: 0 0 1.2em .3em var(--blue);
+    }
+
+    .offer-item[data-no="3"] .offer-dash {
+        background: linear-gradient(135deg, var(--blue-lite), var(--red-lite));
+    }
+
+    .offer-item[data-no="3"] .offer-semititle {
+        -webkit-text-stroke: .05rem var(--blue);
+    }
+
+    .offer-item[data-no="4"] .offer-circle {
+        background-image: radial-gradient(#000 55%, var(--red));
+        box-shadow: 0 0 1.2em .3em var(--red);
+    }
+
+    .offer-item[data-no="4"] .offer-dash {
+        background: linear-gradient(135deg, var(--red-lite), #1110);
+    }
+
+    .offer-item[data-no="4"] .offer-semititle {
+        -webkit-text-stroke: .05rem var(--red);
+    }
+
+
+
+    /* OLD STYLES */
 
     .section-title {
         font-family: Audiowide, 'Courier New', Courier, monospace;
